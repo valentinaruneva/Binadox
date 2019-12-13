d0f2c402e93fba9bd3a348bd881bb13f1df1b7cd
Valya Runeva <v.runeva@zenit.ru>
2019-12-13 16:44:39 +0300

    add material

---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@NgModule({
+  exports: [
+    MatSidenavModule
+  ]
+})
+export class MaterialModule { }
